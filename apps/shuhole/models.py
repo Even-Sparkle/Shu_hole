60fb00315975b6805db034d682b90222cca4de19
Even-Sparkle <1658747506@qq.com>
2025-02-12 22:52:38 +0800

    database start

--- apps/shuhole/models.py
+++ apps/shuhole/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-# 用户id，用户名，手机号，密码，email，照片
+# 用户id，用户名username，手机号phone，密码password，邮箱email，照片image
+
+class RegisterUser(models.Model):
+    username = models.CharField(max_length=20, blank=False, unique=True)
+    password = models.CharField(max_length=20, blank=False, unique=True)
+
+
